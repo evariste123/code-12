46a8e3f14c6d39e86ef1d8565428d96fe900ae6d
evarite123 <bapfekureraevariste@gmail.com>
2025-12-14 10:09:46 +0200

    change 1

---
+++ index6.js
@@ -0,0 +1,13 @@
+let formID=document.getElementById("formID");
+formID.addEventListener(('submit'),(e)=>{
+    e.preventDefault();
+    {
+        const Email=document.getElementById("Email").value;
+    }
+    {
+        console.log("your email:",Email);
+    }
+    {
+        alert("Get your resources from our wwebsite");
+    }
+})
